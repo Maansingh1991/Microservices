467affa2790a86f3aa22f2f34a4f161d405622c2
Maansingh1991 <32849459+Maansingh1991@users.noreply.github.com>
2018-09-17 11:05:47 +0530

    Intial commit
    Intial commit

---
+++ ecom/src/test/java/com/ecommerce/ecom/EcomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ecommerce.ecom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
