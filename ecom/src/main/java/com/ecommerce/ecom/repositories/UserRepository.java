83c4e7ebde6185249f407d56038838f635d39942
Satyam Sharma <satyam.sharma@nagarro.com>
2018-10-09 20:34:10 +0530

    resolving the bean creation exception

--- ecom/src/main/java/com/ecommerce/ecom/repositories/UserRepository.java
+++ ecom/src/main/java/com/ecommerce/ecom/repositories/UserRepository.java
@@ -3,9 +3,10 @@ package com.ecommerce.ecom.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ecommerce.ecom.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	public List<User> findAll();
--- ecom/src/main/java/com/ecommerce/ecom/services/impl/UserServiceImpl.java
+++ ecom/src/main/java/com/ecommerce/ecom/services/impl/UserServiceImpl.java
@@ -5,12 +5,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerce.ecom.entities.User;
 import com.ecommerce.ecom.repositories.UserRepository;
 
 
-
+@Transactional
 @Service
 public class UserServiceImpl implements UserDetailsService {
 
