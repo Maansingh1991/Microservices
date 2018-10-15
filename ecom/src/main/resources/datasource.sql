55ab441330f2670c97ebe73cd42b0975285dea0c
Satyam Sharma <satyam.sharma@nagarro.com>
2018-10-15 22:18:07 +0530

    datasource.sql property file kept separate for the datasources changes even in future

---
+++ ecom/src/main/resources/datasource.sql
@@ -0,0 +1,9 @@
+# create and drop tables and sequences, loads import.sql
+spring.jpa.hibernate.ddl-auto=update
+
+# MySql settings
+#
+spring.datasource.url=jdbc:mysql://localhost:3306/ecomm?useSSL=false
+spring.datasource.username=root
+spring.datasource.password=Maan.singh1991
+spring.datasource.driver-class-name=com.mysql.jdbc.Driver
